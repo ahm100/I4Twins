fc4602db4c0a9b922e38a1f13b3be38de6b0f937
KOOROSH130190\a.sadegh <ahmad.sadegh@gmail.com>
2026-07-05 12:07:18 +0330

    Add Domain entities and value objects

---
+++ I4Twins.Domain/Entities/Reading.cs
@@ -0,0 +1,34 @@
+﻿namespace I4Twins.Domain.Entities;
+
+public class Reading
+{
+    public int Id { get; private set; }
+    public string DeviceId { get; private set; }
+    public string Metric { get; private set; }
+    public DateTime Timestamp { get; private set; }
+    public double Value { get; private set; }
+    public long Seq { get; private set; }
+
+    public Reading(string deviceId, string metric, DateTime timestamp, double value, long seq)
+    {
+        if (string.IsNullOrWhiteSpace(deviceId))
+            throw new ArgumentException("DeviceId is required.", nameof(deviceId));
+        if (string.IsNullOrWhiteSpace(metric))
+            throw new ArgumentException("Metric is required.", nameof(metric));
+        if (timestamp.Kind != DateTimeKind.Utc)
+            throw new ArgumentException("Timestamp must be UTC.", nameof(timestamp));
+        if (double.IsNaN(value) || double.IsInfinity(value))
+            throw new ArgumentException("Value is invalid.", nameof(value));
+        if (seq < 0)
+            throw new ArgumentException("Seq must be positive.", nameof(seq));
+
+        DeviceId = deviceId;
+        Metric = metric;
+        Timestamp = timestamp;
+        Value = value;
+        Seq = seq;
+    }
+
+  
+    public string GetIdentityKey() => $"{DeviceId}_{Metric}_{Timestamp:O}_{Seq}";
+}
---
+++ I4Twins.Domain/Interfaces/IReadingRepository.cs
@@ -0,0 +1,10 @@
+﻿using I4Twins.Domain.Entities;
+
+namespace I4Twins.Domain.Interfaces;
+
+public interface IReadingRepository
+{
+    Task AddAsync(Reading reading);
+    Task<List<Reading>> GetByFilterAsync(string deviceId, string metric, DateTime from, DateTime to);
+    Task<int> GetTotalStoredCountAsync();
+}
